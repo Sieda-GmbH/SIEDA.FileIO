7ea9281c2cef97e52006d8defc632d8dbd9c09c3
Elias Heydrich <heydrich@sieda.com>
2021-07-14 14:10:07 +0200

    Net5.0

--- BUILD_RELEASE.ps1
+++ BUILD_RELEASE.ps1
@@ -1,6 +1,8 @@
 cd Lib
+dotnet build --configuration Release --framework net5.0
 dotnet build --configuration Release --framework net462
 dotnet build --configuration Release --framework net48
 dotnet build --configuration Release --framework netcoreapp3.1
 dotnet build --configuration Release --framework netstandard2.1
 dotnet pack  --configuration Release --output ..\NuGetPackage
+cd "$($PSScriptRoot)"
