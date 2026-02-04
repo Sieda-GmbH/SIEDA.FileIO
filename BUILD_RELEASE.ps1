6d7ad05876d10e2f6acf29fa0229779e7286824b
Elias Heydrich <heydrich@sieda.com>
2026-02-04 12:12:16 +0100

    new version with a NET8 build

--- BUILD_RELEASE.ps1
+++ BUILD_RELEASE.ps1
@@ -1,8 +1,19 @@
-cd Lib
-dotnet build --configuration Release --framework net6.0
-dotnet build --configuration Release --framework net462
-dotnet build --configuration Release --framework net48
-dotnet build --configuration Release --framework netcoreapp3.1
-dotnet build --configuration Release --framework netstandard2.1
-dotnet pack  --configuration Release --output ..\NuGetPackage
-cd "$($PSScriptRoot)"
+$ErrorActionPreference = "Stop"
+try{
+   cd Lib
+   dotnet build --configuration Release --framework net8.0
+   dotnet build --configuration Release --framework net6.0
+   dotnet build --configuration Release --framework net462
+   dotnet build --configuration Release --framework net48
+   dotnet build --configuration Release --framework netcoreapp3.1
+   dotnet build --configuration Release --framework netstandard2.1
+   dotnet pack  --configuration Release --output ..\NuGetPackage
+   cd $PSScriptRoot
+} catch {
+   $errorMsg = $_.Exception.Message
+   Write-Host "`nExiting script with return code 999 due to:" -ForegroundColor Red
+   Write-Host "$($errorMsg)`n" -ForegroundColor Red
+   cd $PSScriptRoot
+   exit 999
+}
+exit 0
